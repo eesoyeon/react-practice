10cfac26ff30b6d5eb0b77377a01b1168eeea1ea
Soyeon Lee <pink731p@gmail.com>
2024-10-26 15:44:42 +0900

    Feat: useReducer

--- hooks-tutorial/src/Couter.jsx
+++ hooks-tutorial/src/Couter.jsx
@@ -1,16 +1,24 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { value: state.value + 1 };
+        case 'DECREMENT':
+            return { value: state.value - 1 };
+        default:
+            return state;
+    }
+}
 
 const Counter = () => {
-    const [count, setCount] = useState(0);
-
-    const onClick = () => {
-        setCount(count + 1);
-    };
+    const [state, dispatch] = useReducer(reducer, { value: 0 });
 
     return (
         <div>
-            <div>{count}</div>
-            <button onClick={onClick}>+</button>
+            <div>현재 카운터 값: {state.value}</div>
+            <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
+            <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
         </div>
     );
 };
